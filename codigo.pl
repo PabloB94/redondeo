13b9d9b9524d868d84f1269b5db87e5848cc9da8
Pablo B <PabloB94@users.noreply.github.com>
2018-05-02 19:49:14 +0200

    Add files via upload

---
+++ codigo.pl
@@ -0,0 +1,32 @@
+:- use_package(trace).
+
+%cero.
+
+nat(0).
+nat(s(N)) :-
+	nat(N).
+
+less_or_equal(0,X) :- nat(X).
+less_or_equal(s(X),s(Y)) :-
+	less_or_equal(X,Y).
+
+last([X]) :- X.
+last([_|Y]) :- last(Y).
+
+%redondeo() :-.
+
+%numeroOriginal(PE, Coma, PD) :-.
+
+redondearDecimal(NI, redondeoUnidad, NF) :-
+	redondearUnidades(NI, NF).
+redondearDecimal(NI, redondeoDecima, NF) :-
+	redondearDecimas(NI, NF).
+redondearDecimal(NI, redondeoCentesima, NF) :-
+	redondearCentesimas(NI, NF).
+
+redondearDecimas(NI, NF) :-
+	redondearDecimas([_|T], NF)
+	PD = last(NI),
+	[D|T] = PD,
+	T = [C|T2],
+	less_or_equal(s(s(s(s(s(0))))), C).
