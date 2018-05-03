b5e2b5107d9422ee2ce3204aca8a7946c8254bf7
Mario <mluengo96@users.noreply.github.com>
2018-05-03 17:38:53 +0200

    Añadido caso mayor o igual

--- codigo2.pl
+++ codigo2.pl
@@ -6,9 +6,13 @@ nat(0).
 nat(s(N)) :-
 	nat(N).
 
-less_or_equal(0,X) :- nat(X).
-less_or_equal(s(X),s(Y)) :-
-	less_or_equal(X,Y).
+ls_or_equal(0,X) :- nat(X).
+ls_or_equal(s(X),s(Y)) :-
+	ls_or_equal(X,Y).
+
+gt_or_equal(X,0) :- nat(X).
+gt_or_equal(s(X),s(Y)) :-
+	more_or_equal(X,Y).
 
 first([H|_],H).
 
@@ -16,9 +20,9 @@ last([X],X).
 last([_|Z],X) :-
 	last(Z,X).
 
-sum(zero, A, A).
-sum(s(A), B, s(C)) :-
-	sum(A, B, C).
+sum(0, X, X).
+sum(s(X), s(0), s(Z)) :-
+	sum(X, s(0), Z).
 
 %redondeo() :-.
 
@@ -33,8 +37,14 @@ redondearDecimal(NI, redondeoCentesima, NF) :-
 
 compU([H|T],NF) :-
 	last(T, S),
-	less_or_equal(S, s(s(s(s(s(0)))))),
+	ls_or_equal(S, s(s(s(s(0))))),
 	first([H|_], NF).
+
+compU([H|T],NF) :-
+	last(T, S),
+	gt_or_equal(S, s(s(s(s(s(0)))))),
+	first([H|_], X),
+	sum(X, s(0), NF).
 
 %redondearUnidades(PD, NF) :-
 %	less_or_equal(PD, s(s(s(s(s(0)))))).
