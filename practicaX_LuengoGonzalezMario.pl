66fe571ca580da9dd8d8cdadac951695fc533f14
Mario <mluengo96@users.noreply.github.com>
2018-05-24 17:59:22 +0200

    Add files via upload

---
+++ practicaX_LuengoGonzalezMario.pl
@@ -0,0 +1,8 @@
+:- use_package(trace).
+
+alumno_prode('Luengo','Gonzalez','Mario','W140280').
+alumno_prode('Santos','Gonzalez','Sergio','W140180').
+alumno_prode('Beltran','de Casso','Pablo','Y160454').
+
+cierre(X,Y) :-
+	
