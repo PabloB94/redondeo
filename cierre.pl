7aafeb0114708f40dfde660b5121c60a744573b3
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2018-06-05 23:25:22 +0200

    Cambiados not por \+
    Nuevo estandar de ISO Prolog

--- cierre.pl
+++ cierre.pl
@@ -1,8 +1,4 @@
-% Universidad Politecnica de Madrid
-% Programacion declarativa: logica y restricciones
-% 6F1M
-
-% INTEGRANTES:
+% Autores:
 alumno_prode('Luengo','Gonzalez','Mario','W140280').
 alumno_prode('Santos','Gonzalez','Sergio','W140180').
 alumno_prode('Beltran','de Casso','Pablo','Y160454').
@@ -11,29 +7,25 @@ cierre(LinkList,Result):-
 	noRepeatedElements(LinkList),
 	findChain(LinkList,LinkList,Result).
 
-% noRepeatedElements/1: relacion que es cierta sii no hay ningun eslabon repetido en su primer argumento.
 noRepeatedElements([]).
 noRepeatedElements([X|Xs]):-
 	checkEqual(X,Xs),
     	noRepeatedElements(Xs).
 
 
-% checkEqual/2: relacion que es cierta sii el eslabon X no se
-% repite en la listade eslabones.
 checkEqual(_,[]).
 checkEqual(X,[Y|Xs]):- 
 	noRepeatedLinks(X,Y),
 	checkEqual(X,Xs).
 
 
-% equalLink/2: relacion que es cierta sii los dos eslabones tienen los mismos extremos.
 noRepeatedLinks(A,B):-
 	arg(1,A,A1),
 	arg(2,A,A2),
 	arg(1,B,B1),
 	arg(2,B,B2),
-	((not(A1==B1),not(A2==B2));
-	(not(A1==B2),not(A2==B1))).
+	((\+(A1==B1),\+(A2==B2));
+	(\+(A1==B2),\+(A2==B1))).
 
 
 findChain([X|_],LinkList,Result):-
@@ -94,7 +86,7 @@ deleteDuplicates([X|Xs], NoDuplicates):-
 	
 deleteDuplicates([X|Xs], [X|NoDuplicates]):-
 	permutation(X,Y),
-	not(findDuplicates(Y,Xs)),
+	\+(findDuplicates(Y,Xs)),
 	deleteDuplicates(Xs,NoDuplicates).
 
 
