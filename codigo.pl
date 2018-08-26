9bee4fbf3827a806b47ba273bd7cb12852e094d5
Pablo B <PabloB94@users.noreply.github.com>
2018-08-26 21:51:22 +0100

    Update codigo.pl

--- codigo.pl
+++ codigo.pl
@@ -1,9 +1,5 @@
 :- use_package(trace).
 
-alumno_prode('Luengo','Gonzalez','Mario','W140280').
-alumno_prode('Santos','Gonzalez','Sergio','W140180').
-alumno_prode('Beltran','de Casso','Pablo','Y160454').
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%% EJERCICIO 1 %%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
